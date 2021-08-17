0c68bf93838e14ad43395c997b653e62a6c3e489
oytundinc <oytun.dinc@gmail.com>
2021-08-17 17:21:07 +0300

    Duration timer added.

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../button/Button.css'
 
+
 function Button({children,...rest}) {
     return (
         <button className="btn" {...rest}>{children}</button>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,13 @@
 import Button from "../button/Button"
+import Timer from "../timer/Timer"
 import "../navbar/Navbar.css"
 
 const Navbar = () => {
     return (
         <div className="navbar">
+            <Timer></Timer>
             <Button>New Game</Button>
+            
         </div>
         
     )
